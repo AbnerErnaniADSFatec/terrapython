8f03b109f324620a2f5ec3b7280305594836ff37
AbnerErnaniADSFatec <abiner.anjos@gmail.com>
2019-07-30 11:23:58 -0300

    update-analysis-controll

--- an_monthly.py
+++ an_monthly.py
@@ -1,5 +1,5 @@
 meses = {1:'Janeiro',2:'Fevereiro',3:'Março',4:'Abril',5:'Maio',6:'Junho',7:'Julho',8:'Agosto',9:'Setembro',10:'Outubro',11:'Novembro',12:'Dezembro'}
-dic = {1:31,2:0,3:31,4:30,5:31,6:30,7:31,8:31,9:30,10:31,11:30,12:31}
+dias = {1:31,2:0,3:31,4:30,5:31,6:30,7:31,8:31,9:30,10:31,11:30,12:31}
 an_mes = get_analysis_date().month
 ano = get_analysis_date().year
 bissexto = None
@@ -10,30 +10,16 @@ if ano % 4 == 0:
 	    else: bissexto = False
 	else: bissexto = True
 else: bissexto = False
-if bissexto: dic[2] = 29
-else: dic[2] = 28
+if bissexto: dias[2] = 29
+else: dias[2] = 28
 
-geocodigo = get_value("geocodigo")
-nome_municipio = get_value("nome1")
-uf = get_value("uf")
-maxima = grid.zonal.max("monthly", 0)
-media = grid.zonal.mean("monthly", 0)
+maxima = grid.zonal.max("monthly")
+media = grid.zonal.mean("monthly")
 mes = meses[an_mes]
 ano = get_analysis_date().year
 
-if dic[an_mes] != get_analysis_date().day:
-    geocodigo = None
-    nome_municipio = None
-    uf = None
-    maxima = None
-    media = None
-    mes = None
-    ano = None
-
-add_value("geocodigo", geocodigo)
-add_value("nome_municipio", nome_municipio)
-add_value("uf", uf)
-add_value("maxima", maxima)
-add_value("media", media)
-add_value("mes", mes)
-add_value("ano", ano)
+if dias[an_mes] == get_analysis_date().day:
+    add_value("maxima", maxima)
+    add_value("media", media)
+    add_value("mes", mes)
+    add_value("ano", ano)
